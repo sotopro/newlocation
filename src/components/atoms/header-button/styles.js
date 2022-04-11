4970947f32a2adb939fe075b2b21ec752bb6885c
APPLE <apple@MacBook-Pro-de-APPLE.local>
2022-04-11 18:29:07 -0500

    [feature] - create header button component in navigation & add

---
+++ src/components/atoms/header-button/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../../constants";
+
+export const styles = StyleSheet.create({
+    textButton: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: COLORS.primaryColor
+    },
+});
