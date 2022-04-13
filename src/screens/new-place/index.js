43cd7ae456ad6acb93d12dc27aa36f330082580d
Daniel Soto <dasg.1993@gmail.com>
2022-04-13 17:31:41 -0500

    [feature] - create image picker selector & implement in new place screen

--- src/screens/new-place/index.js
+++ src/screens/new-place/index.js
@@ -1,36 +1,41 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, ScrollView, Button } from "react-native";
-import { styles } from "./styles";
-import { COLORS } from "../../constants";
-import { useDispatch } from "react-redux";
-import { placeActions } from "../../store/action";
+import React, {useState} from 'react';
+import {View, Text, TextInput, ScrollView, Button} from 'react-native';
+import {styles} from './styles';
+import {COLORS} from '../../constants';
+import {useDispatch} from 'react-redux';
+import {placeActions} from '../../store/action';
+import ImageSelector from '../../components/molecules/image-selector';
 
-const NewPlace = ({ navigation }) => {
-    const dispatch = useDispatch();
-    const [name, setName] = useState("");
+const NewPlace = ({navigation}) => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
 
-    const handleNameChange = (text) => setName(text);
-    const handleSavePlace = () => {
-        dispatch(placeActions.addPlace(name));
-        navigation.navigate('Place');
-    }
-    return (
-        <ScrollView>
-            <View style={styles.container}>
-                <Text style={styles.label}>Name</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={handleNameChange}
-                    value={name}
-                    />
-                <Button
-                    title="Save place"
-                    onPress={() => handleSavePlace()}
-                    color={COLORS.primaryColor}
-                    />
-            </View> 
-        </ScrollView>
-)
-}
+  const handleNameChange = text => setName(text);
+  const handleSavePlace = () => {
+    dispatch(placeActions.addPlace(name));
+    navigation.navigate('Place');
+  };
+  const handleOnImage = uri => {
+    console.warn(uri);
+  };
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.label}>Name</Text>
+        <ImageSelector onImage={handleOnImage} />
+        <TextInput
+          style={styles.input}
+          onChangeText={handleNameChange}
+          value={name}
+        />
+        <Button
+          title="Save place"
+          onPress={() => handleSavePlace()}
+          color={COLORS.primaryColor}
+        />
+      </View>
+    </ScrollView>
+  );
+};
 
 export default NewPlace;
